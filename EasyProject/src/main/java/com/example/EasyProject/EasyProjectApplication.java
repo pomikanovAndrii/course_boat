a9db7df01977a2502f2fb0d7e7ad876f6f61727c
pomikanovAndrii <149969501+pomikanovAndrii@users.noreply.github.com>
2023-11-13 08:03:42 +0200

    ulpoad project

---
+++ EasyProject/src/main/java/com/example/EasyProject/EasyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EasyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyProjectApplication.class, args);
+	}
+
+}
